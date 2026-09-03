bd459e2c2c48c9078809fac236be9c59d5a2931c
BradleyZhang <2111976953@qq.com>
2026-09-03 09:09:24 +0800

    parser:支持 逗号运算符

--- lox/parser.go
+++ lox/parser.go
@@ -11,7 +11,9 @@
 //                | "+"  | "-"  | "*" | "/" ;
 
 // 优先级由低到高，每个规则只匹配>=自身优先级的表达式
+// 以 1+2*3为例， term 要求左右是高一级的完整factor，这样把表达式切成 (1) + (2*3) ，可以理解为低优先级的决定在哪里切开，然后把左右交给高优先级
 // expression     → equality ;
+// comma          → equality ((",") equality )* ; (逗号运算符)
 // equality       → comparison ( ( "!=" | "==" ) comparison )* ; (等于)
 // comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ; （比较）
 // term           → factor ( ( "-" | "+" ) factor )* ; （加减）
@@ -21,6 +23,15 @@
 // primary        → NUMBER | STRING | "true" | "false" | "nil"
 //                | "(" expression ")" ; （字面量和括号表达式）
 
+// 优先级从低到高
+// | Name              | Operators         | Associates    |
+// | ----------------- | ----------------- | ------------- |
+// | Comma 逗号运算符    | `,`               | Left 左结合   |
+// | Equality  等于    | `==` `!=`         | Left  左结合  |
+// | Comparison  比较  | `>` `>=` `<` `<=` | Left  左结合  |
+// | Term  加减运算    | `-` `+`           | Left  左结合  |
+// | Factor   乘除运算 | `/` `*`           | Left  左结合  |
+// | Unary  一元运算符 | `!` `-`           | Right  右结合 |
 package lox
 
 type Parser struct {
@@ -48,7 +59,23 @@ func (p *Parser) Parse() Expr {
 }
 
 func (p *Parser) expression() (Expr, error) {
-	return p.equality()
+	return p.comma()
+}
+
+func (p *Parser) comma() (Expr, error) {
+	expr, err := p.equality()
+	if err != nil {
+		return nil, err
+	}
+	for p.match(Comma) {
+		operator := p.previous()
+		right, err := p.equality()
+		if err != nil {
+			return nil, err
+		}
+		expr = Binary{expr, operator, right}
+	}
+	return expr, nil
 }
 
 func (p *Parser) equality() (Expr, error) {
